efd4b8e804ec5df28bda5c7353047b715cf918c0
Ayushman Tripathi <ayushmantripathi7724@gmail.com>
2023-05-23 00:13:11 +0530

    luci-app-olsr: fix formatting

--- applications/luci-app-olsr-js/htdocs/luci-static/resources/model/cbi/olsr/olsrddisplay.js
+++ applications/luci-app-olsr-js/htdocs/luci-static/resources/model/cbi/olsr/olsrddisplay.js
@@ -1,21 +1,20 @@
-'use strict';
-'require view';
-'require form';
+"use strict";
+"require view";
+"require form";
 
 return view.extend({
-	render: function() {
-			var m, s, o;
+	render: function () {
+		var m, s, o;
 
-			m = new form.Map("luci_olsr", _("OLSR - Display Options"))
+		m = new form.Map("luci_olsr", _("OLSR - Display Options"));
 
-			s = m.section(form.TypedSection, "olsr")
-			s.anonymous = true
-			
-			res = s.option(form.Flag, "resolve", _("Resolve"),
-											_("Resolve hostnames on status pages. It is generally safe to allow this, but if you use public IPs and have unstable DNS-Setup then those pages will load really slow. In this case disable it here."))
-			res.default = "0"
-			res.optional = true
+		s = m.section(form.TypedSection, "olsr");
+		s.anonymous = true;
 
-			return m.render();
+		res = s.option(form.Flag, "resolve", _("Resolve"), _("Resolve hostnames on status pages. It is generally safe to allow this, but if you use public IPs and have unstable DNS-Setup then those pages will load really slow. In this case disable it here."));
+		res.default = "0";
+		res.optional = true;
+
+		return m.render();
 	},
 });
--- applications/luci-app-olsr-js/htdocs/luci-static/resources/model/cbi/olsr/olsrdhna6.js
+++ applications/luci-app-olsr-js/htdocs/luci-static/resources/model/cbi/olsr/olsrdhna6.js
@@ -1,31 +1,28 @@
-'use strict';
-'require view';
-'require form';
-'require	uci';
+"use strict";
+"require view";
+"require form";
+"require	uci";
 
 return view.extend({
-	render: function() {
+	render: function () {
+		var ipv = uci.get_first("olsrd", "olsrd", "IpVersion") || "4";
 
-			var ipv =  uci.get_first("olsrd", "olsrd", "IpVersion") || "4";
+		mh = new form.Map("olsrd6", translate("OLSR - HNA6-Announcements"), translate("Hosts in an OLSR routed network can announce connectivity " + "to external networks using HNA6 messages."));
 
-			mh = new form.Map("olsrd6", translate("OLSR - HNA6-Announcements"), translate("Hosts in an OLSR routed network can announce connectivity " +
-				"to external networks using HNA6 messages."))
-			
-		 hna6 = mh.section(form.TypedSection, "Hna6", translate("Hna6"), translate("IPv6 network must be given in full notation, " +
-				"prefix must be in CIDR notation."))
-			hna6.addremove = true
-			hna6.anonymous = true
-			hna6.template  = "cbi/tblsection"
-		
-			net6 = hna6.option(form.Value, "netaddr", translate("Network address"))
-			net6.datatype = "ip6addr"
-			net6.placeholder = "fec0:2200:106:0:0:0:0:0"
-			net6.default = "fec0:2200:106:0:0:0:0:0"
-			msk6 = hna6.option(form.Value, "prefix", translate("Prefix"))
-			msk6.datatype = "range(0,128)"
-			msk6.placeholder = "128"
-			msk6.default = "128"
-			
-			return mh.render();
+		hna6 = mh.section(form.TypedSection, "Hna6", translate("Hna6"), translate("IPv6 network must be given in full notation, " + "prefix must be in CIDR notation."));
+		hna6.addremove = true;
+		hna6.anonymous = true;
+		hna6.template = "cbi/tblsection";
+
+		net6 = hna6.option(form.Value, "netaddr", translate("Network address"));
+		net6.datatype = "ip6addr";
+		net6.placeholder = "fec0:2200:106:0:0:0:0:0";
+		net6.default = "fec0:2200:106:0:0:0:0:0";
+		msk6 = hna6.option(form.Value, "prefix", translate("Prefix"));
+		msk6.datatype = "range(0,128)";
+		msk6.placeholder = "128";
+		msk6.default = "128";
+
+		return mh.render();
 	},
 });
